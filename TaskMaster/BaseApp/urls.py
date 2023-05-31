86cecaf439839d03af463542b695e6f155066846
Filipek204 <113829682+Filipek204@users.noreply.github.com>
2023-05-31 22:52:01 +0200

    al CRUD and started styling home page and navbar

--- TaskMaster/BaseApp/urls.py
+++ TaskMaster/BaseApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import addList, addItem, home
+from .views import addList, addItem, home, updateList, updateItem, deleteList, deleteItem
 urlpatterns = [
     path('home/', home, name="home"),
     path('add-list/', addList, name="add-list"),
-    path('add-item/<str:pk>', addItem, name="add-item"),
+    path('update-list/<str:pk>/', updateList, name="update-list"),
+    path('delete-list/<str:pk>/', deleteList, name="delete-list"),
+    path('add-item/<str:pk>/', addItem, name="add-item"),
+    path('update-item/<str:pk>/', updateItem, name="update-item"),
+    path('delete-item/<str:pk>/', deleteItem, name="delete-item"),
 ]
--- TaskMaster/BaseApp/views.py
+++ TaskMaster/BaseApp/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import List, ListItems
 from .forms import ListForm, ListItemsForm
-# # dodac wypisywanie list i update ich,
-# # pokazywanie kazdej listy z osobna
+
 
 def home(request):
-    lists=List.objects.all()
-    context={
-        'lists':lists,
+    lists = List.objects.all()
+    context = {
+        'lists': lists,
     }
-    return render(request,"BaseApp/home.html", context)
+    return render(request, "BaseApp/home.html", context)
+
+####################################################### List CRUD ###############################################################
+
 
 def addList(request):
     form = ListForm()
@@ -21,7 +23,38 @@ def addList(request):
     context = {
         "form": form
     }
-    return render(request, "BaseApp/add-list.html", context)
+    return render(request, "BaseApp/list-form.html", context)
+
+
+def updateList(request, pk):
+    list = List.objects.get(id=pk)
+    form = ListForm(instance=list)
+
+    if request.method == "POST":
+        form = ListForm(request.POST, instance=list)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {
+        "form": form,
+        "list": list
+    }
+    return render(request, "BaseApp/list-form.html", context)
+
+
+def deleteList(request, pk):
+    list = List.objects.get(id=pk)
+    items = ListItems.objects.filter(list=list)
+    if request.method == "POST":
+        list.delete()
+        items.delete()
+        return redirect('home')
+    context = {
+        "list": list
+    }
+    return render(request, "BaseApp/delete-list.html", context)
+
+####################################################### Item CRUD ###############################################################
 
 
 def addItem(request, pk):
@@ -30,18 +63,37 @@ def addItem(request, pk):
     if request.method == "POST":
         form = ListItemsForm(request.POST)
         if form.is_valid():
-            form.instance.list=List.objects.get(id=pk)
+            form.instance.list = List.objects.get(id=pk)
             form.save()
     context = {
         "form": form,
         "items": items
     }
-    return render(request, "BaseApp/add-item.html", context)
+    return render(request, "BaseApp/items.html", context)
 
 
-# def listView(request):
-#     # items = ToDoItems.objects.get(id=id)
-#     # context = {
-#     #     "items": items
-#     # }
-#     return render(request, "list.html")
+def updateItem(request, pk):
+    item = ListItems.objects.get(id=pk)
+    form = ListItemsForm(instance=item)
+
+    if request.method == "POST":
+        form = ListItemsForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {
+        "form": form,
+        "item": item
+    }
+    return render(request, "BaseApp/items.html", context)
+
+
+def deleteItem(request, pk):
+    item = ListItems.objects.get(id=pk)
+    if request.method == "POST":
+        item.delete()
+        return redirect('home')
+    context = {
+        "item": item
+    }
+    return render(request, "BaseApp/delete-item.html", context)
